c2c439ffd44a5d39d719fb66911ea24692afed99
Pammy-Sah <pammykumari.bth05@gmail.com>
2024-11-16 05:14:29 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="pammykuamri.bth05@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+//accountId=2
+console.log(accountId);
+
+accountEmail="hc@hc.com"
+accountPassword="23232"
+accountCity="bengluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello")  //we are using node js not browsers
+
+console.log(3+3);
